f64dcef560fc6d4c37b106ae3c0f40b5e0a5e70e
timothy0618 <87256320+timothy0618@users.noreply.github.com>
2024-03-19 15:48:59 -0700

    Text to speech testing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import {useRouter} from 'next/router'
 import { IntlProvider } from "react-intl";
+import 'regenerator-runtime/runtime';
 
 import en from "../i18n/en.json";
 import zh from "../i18n/zh.json";
