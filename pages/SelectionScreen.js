0c6358a64560c3083f8ff803e9fb3b942c70edbd
Jennica <jsask1@my.bcit.ca>
2024-04-17 19:06:45 -0700

    added page for nickname and for selecting games

--- pages/SelectionScreen.js
+++ pages/SelectionScreen.js
@@ -1,26 +1,16 @@
-import styles from "@/styles/Home.module.css";
+import styles from "@/styles/SelectionScreen.module.css";
 import { useRouter } from "next/router";
 import { FormattedMessage, useIntl } from "react-intl";
 import HeadArea from "@/Components/HeadArea";
 import Link from "next/link";
 import Image from 'next/image';
-import { useState } from "react";
 
-export default function Home({ dir }) {
+export default function SelectionScreen({ dir }) {
   const { locales } = useRouter();
   const intl = useIntl();
-  const router = useRouter();
-
-  const [nickname, setNickname] = useState("");
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    router.push("./SelectionScreen");
-  };
 
   const title = intl.formatMessage({ id: "page.home.head.title" });
   const description = intl.formatMessage({ id: "page.home.head. description" });
-
-  
   return (
     <>
       <HeadArea title={title} description={description} />
@@ -36,19 +26,19 @@ export default function Home({ dir }) {
 
       <main className={`${styles.main}`}>
         <div className={styles.heading}>
-          <h1 className={styles.title}>Create Nickname</h1>
-
-          <form className={styles.subtitleContainer} onSubmit={handleFormSubmit}>
-            <input type="text"
-              className={`${styles.subtitle}`}
-              value={nickname}
-              onChange={(e) => setNickname(e.target.value)} 
-            />
-            <button className={`button-font-style-1 ${styles.button}`} type="submit">Confirm!</button>
-          </form>
+          <h1 className={styles.title}>Pocket Pals</h1>
+          <div className={styles.subtitleContainer}>
+            <p className={`description-font-styles ${styles.subtitle}`}>An Animal Learning App for Kids!</p>
+          </div>
         </div>
 
-        <Image className={styles.mascot} src="./image/Mascot.svg" width={500} height={400} />
+        <Image src="./image/Mascot.svg" width={500} height={400} />
+
+        <div className={styles.buttonContainer}>
+          <button className={`button-font-style-1 ${styles.button}`}><Link href="/">Learn!</Link></button>
+          <button className={`button-font-style-1 ${styles.button}`}><Link href="/QuizGame">Quiz!</Link></button>
+          <button className={`button-font-style-1 ${styles.button}`}><Link href="/SelectQuizTopic">Mix and Match!</Link></button>
+        </div>
       </main>
     </>
   );
