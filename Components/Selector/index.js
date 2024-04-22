ce707477f170f32b4206b1f8bd3b7f31a15a4954
timothy0618 <87256320+timothy0618@users.noreply.github.com>
2024-04-22 12:06:15 -0700

    Create a setting page for testing.

--- Components/Selector/index.js
+++ Components/Selector/index.js
@@ -1,7 +1,6 @@
 import style from './Selector.module.css'
 
 export default function (prop) {
-    console.log(prop)
     const onChange = (e) => { prop.onChange(e.target.value) }
     return (<>
         <div className={style.selector}>
