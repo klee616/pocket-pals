eb4b064358882c31847789d1188f26171f471704
timothy0618 <87256320+timothy0618@users.noreply.github.com>
2024-04-27 00:28:08 -0700

    update home menu page

--- pages/HomeMenu.js
+++ pages/HomeMenu.js
@@ -1,11 +1,9 @@
 import styles from "@/styles/HomeMenu.module.css";
 import { useRouter } from "next/router";
-import { FormattedMessage, useIntl } from "react-intl";
 import MenuButton from "@/Components/MenuButton/";
 import Footer from "@/Components/Footer/"
 import Header from "@/Components/Header/"
 import HeadArea from "@/Components/HeadArea";
-import Image from "next/image";
 
 export default function HomeMenu({ dir }) {
   const { locales } = useRouter();
@@ -14,22 +12,22 @@ export default function HomeMenu({ dir }) {
 
   const headTitle = intl.formatMessage({ id: "page.home.head.title" });
   const headDescription = intl.formatMessage({ id: "page.home.head.description" });
-  const title = intl.formatMessage({id: "page.home.title"});
+  const title = intl.formatMessage({ id: "page.home.title" });
   const description = intl.formatMessage({ id: "page.home.description" });
   return (
     <>
-      
+
       <HeadArea title={headTitle} description={headDescription} />
-        <Header />
-        <div className={styles.main}>
-            <h1>{title}</h1>
-            <p>{description}</p>
+      <Header />
+      <div className={styles.main}>
+        <h1>{title}</h1>
+        <p>{description}</p>
 
         <div className={styles.options}>
-        <MenuButton  name="Quiz" image="/image/Panda.png" href="/Quiz" />
-<MenuButton  name="Mix and Match" image="/image/Hamster.png" href="/HomeMenu" />
-<MenuButton  name="Learn" image="/image/Bear.png" href="/Learn" />
-<MenuButton  name="Setting" image="/image/numbat.png" href="/Setting" />
+          <MenuButton name="Quiz" image="/image/Panda.png" href="/Quiz" />
+          <MenuButton name="Mix and Match" image="/image/Hamster.png" href="/HomeMenu" />
+          <MenuButton name="Learn" image="/image/Bear.png" href="/Learn" />
+          <MenuButton name="Setting" image="/image/numbat.png" href="/Setting" />
 
 
 
