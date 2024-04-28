6af4e7b211d8d617e3a92c7a8c6053eedcaa2221
timothy0618 <87256320+timothy0618@users.noreply.github.com>
2024-04-28 15:49:41 -0700

    The mix and match button will go to the home page

--- pages/SelectionScreen.js
+++ pages/SelectionScreen.js
@@ -41,7 +41,7 @@ export default function SelectionScreen({ dir }) {
         
           <Button name={intl.formatMessage({id: "page.selection.screen.learn"})} onClick={()=>buttonSubmit('/Learn')} />
           <Button name={intl.formatMessage({id: "page.selection.screen.quiz"})} onClick={()=>buttonSubmit('/Quiz')} />
-          <Button name={intl.formatMessage({id: "page.selection.screen.mix.and.match"})} onClick={()=>buttonSubmit('/MixAndMatchGame')} />
+          <Button name={intl.formatMessage({id: "page.selection.screen.mix.and.match"})} onClick={()=>buttonSubmit('/')} />
 
         </div>
       </main>
