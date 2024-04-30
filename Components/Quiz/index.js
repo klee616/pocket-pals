f73312a2390064cedd539059adb7d527b98f7cd6
timothy0618 <87256320+timothy0618@users.noreply.github.com>
2024-04-29 17:32:29 -0700

    create History function

--- Components/Quiz/index.js
+++ Components/Quiz/index.js
@@ -6,6 +6,7 @@ import QuizResult from '@/Components/QuizResult';
 import ProgressBar from '@/Components/ProgressBar';
 import Button from '@/Components/Button';
 import { useIntl } from "react-intl";
+import { setCookie, getCookie, hasCookie, deleteCookie } from 'cookies-next';
 
 export default function Qiiz({ category = "all" }) {
     const intl = useIntl();
@@ -18,9 +19,9 @@ export default function Qiiz({ category = "all" }) {
         questionList: []
     });
 
-    const [ nickName, setNickName ] = useState("");
+    const [nickName, setNickName] = useState("");
     useEffect(() => {
-      setNickName(window.sessionStorage.getItem("nickname"));
+        setNickName(window.sessionStorage.getItem("nickname"));
     }, []);
 
     const { locale } = useRouter();
@@ -36,14 +37,24 @@ export default function Qiiz({ category = "all" }) {
             current: resultData.current + 1,
             questionList: [...resultData.questionList, currentOfQuestion[locale]]
         }));
+        console.log(resultData.questionList.length == data.length);
+        if (resultData.questionList.length + 1 == data.length) {
+            saveResultInCookies();
+        }
     };
 
+    const saveResultInCookies = async () => {
+        let historyRecord = getCookie('historyRecord');
+        let historyRecordList = historyRecord ? JSON.parse(historyRecord) : [];
+        historyRecordList.push({ name: nickName, category: category, date: new Date(), scores: Math.round(resultData.totalOfCorrectAnswers / data.length * 100) })
+        setCookie('historyRecord', historyRecordList);
+    }
 
     return (<>
         {resultData.current < data.length && (<>
-        <ProgressBar current={resultData.current  + 1} total={data.length} />
+            <ProgressBar current={resultData.current + 1} total={data.length} />
 
-        <h1 className={`header_font`}>{intl.formatMessage({ id: "page.quiz.question" })} {resultData.current  + 1}</h1>
+            <h1 className={`header_font`}>{intl.formatMessage({ id: "page.quiz.question" })} {resultData.current + 1}</h1>
             <Question data={data[resultData.current]} callBack={getAnswer} />
         </>
         )}
@@ -51,7 +62,7 @@ export default function Qiiz({ category = "all" }) {
         {resultData.questionList.length == data.length &&
             (
                 <><QuizResult resultData={resultData} />
-                <Button name={intl.formatMessage({ id: "page.quiz.play.again" })} onClick={()=>{router.push('/Quiz')}}/>
+                    <Button name={intl.formatMessage({ id: "page.quiz.play.again" })} onClick={() => { router.push('/Quiz') }} />
                 </>
             )
         }
