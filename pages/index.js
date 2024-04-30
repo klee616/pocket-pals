63cab6c0f9b92fd6432e231ee7090e301aa4014d
timothy0618 <87256320+timothy0618@users.noreply.github.com>
2024-04-30 16:08:31 -0700

    add the validation

--- pages/index.js
+++ pages/index.js
@@ -14,16 +14,25 @@ export default function Home({ dir }) {
 
   const [nickname, setNickname] = useState("");
   const handleFormSubmit = (e) => {
-    window.sessionStorage.setItem("nickname", nickname);
     e.preventDefault();
+    if (nickname == null || nickname=="") {
+      setErrorMessage(intl.formatMessage({ id: "page.home.nickname.error.message" }));
+      return;
+    }
+    if(!nickname.match(/^[A-Z]+$/i)){
+      setErrorMessage(intl.formatMessage({ id: "page.home.nickname.invalid.error.message" }));
+      return;
+
+    }
+    window.sessionStorage.setItem("nickname", nickname);
     router.push(locale + "/SelectionScreen");
   };
 
   const headTitle = intl.formatMessage({ id: "page.home.head.title" });
   const headDescription = intl.formatMessage({ id: "page.home.head.description" });
-  const title = intl.formatMessage({id: "page.home.title"});
+  const title = intl.formatMessage({ id: "page.home.title" });
   const description = intl.formatMessage({ id: "page.home.description" });
-
+  const [errorMessage, setErrorMessage] = useState('');
 
   return (
     <>
@@ -36,17 +45,18 @@ export default function Home({ dir }) {
           </div>
         </div>
 
-         <form className={styles.formContainer} onSubmit={handleFormSubmit}>
-          <label className={`label-font-style ${styles.label}`} for="nickName">{intl.formatMessage({id:"page.home.label.nickname"})}</label>
+        <form className={styles.formContainer} onSubmit={handleFormSubmit}>
+          <label className={`label-font-style ${styles.label}`} for="nickName">{intl.formatMessage({ id: "page.home.label.nickname" })}</label>
           <input
             id="nickName"
             type="text"
             value={nickname}
             onChange={(e) => setNickname(e.target.value)}
           />
-          <Button name={intl.formatMessage({id: "page.home.submit"})} onClick={() => { }} />
+          <label className={styles.errorMsg}>{errorMessage}</label>
+          <Button name={intl.formatMessage({ id: "page.home.submit" })} onClick={() => { }} />
         </form>
-        <Image className={styles.mascot} src="./image/Mascot.svg" width={500} height={400} />
+        <Image className={styles.mascot} src="./image/Mascot.svg" width={500} height={400} alt="POCKET PALS" />
 
 
       </main>
