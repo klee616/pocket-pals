54180b2abd26cfd77bb56b45d89d7fdd94dc01c1
timothy0618 <87256320+timothy0618@users.noreply.github.com>
2024-05-03 11:10:49 -0700

    css

--- Components/HeadArea/index.js
+++ Components/HeadArea/index.js
@@ -6,11 +6,11 @@ export default function HeadArea({ title, description }) {
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
-      {/* <meta name="viewport" content="width=430, height:932, initial-scale=1" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link
         href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap"
         rel="stylesheet"
-      /> */}
+      /> 
       <link rel="icon" href="/favicon.ico" />
       <link rel="icon" href="/favicon.ico" hrefLang="x-default" />
       <link rel="icon" href="/favicon.ico" hrefLang="en" />
