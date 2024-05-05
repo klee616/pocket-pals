37544f0dce83beb914eec4d802c47e823cb54204
timothy0618 <87256320+timothy0618@users.noreply.github.com>
2024-05-04 19:27:55 -0700

    update mix and match

--- Components/Matchify/index.js
+++ Components/Matchify/index.js
@@ -2,7 +2,7 @@ import matchifyData from '@/data/matchify.json';
 import { useEffect, useState } from 'react';
 import MatchifyCard from '@/Components/MatchifyCard';
 import style from './matchify.module.css';
-import ProgressBar from '../ProgressBar';
+import ProgressBar from '@Components/ProgressBar';
 
 export default function Matchify() {
     const [data, setData] = useState(matchifyData);
