6f58de66ae768da317441d4e08894764202e6a13
timothy0618 <87256320+timothy0618@users.noreply.github.com>
2024-05-07 13:28:16 -0700

    sound effect

--- Components/QuizResult/index.js
+++ Components/QuizResult/index.js
@@ -2,25 +2,42 @@ import style from "./QuizResult.module.css";
 import QuizResultAnswer from "../QuizResultAnswer";
 import { useIntl } from "react-intl";
 import { setCookie, getCookie, hasCookie, deleteCookie } from 'cookies-next';
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export default function QuizResult({ resultData }) {
     const intl = useIntl();
     const [nickName, setNickName] = useState();
+    const successSoundEffect = useRef();
+    const failSoundEffect = useRef();
+    const playSuccess = () => {
+        if (successSoundEffect.current) {
+            successSoundEffect.current.play();
+        }
+    }
+    const playFail = () => {
+        if (failSoundEffect.current) {
+            failSoundEffect.current.play();
+        }
+    }
 
     useEffect(() => {
         setNickName(window.sessionStorage.getItem("nickname"));
         saveResultInCookies();
+        if(resultData.totalOfCorrectAnswers / resultData.current >= 0.5){
+            playSuccess();
+        }else{
+            playFail();
+        }
     }, []);
     const saveResultInCookies = async () => {
         let historyRecord = getCookie('historyRecord');
         let historyRecordList = historyRecord ? JSON.parse(historyRecord) : [];
         let isRecord = false;
         console.log(historyRecordList);
-        historyRecordList.map((item)=>{
-           if(item.id == resultData.id){
-            isRecord = true; 
-           }
+        historyRecordList.map((item) => {
+            if (item.id == resultData.id) {
+                isRecord = true;
+            }
         });
         if (!isRecord) {
             historyRecordList.push({ id: resultData.id, name: window.sessionStorage.getItem("nickname"), topic: resultData.topic, date: new Date(), scores: Math.round(resultData.totalOfCorrectAnswers / resultData.current * 100) })
@@ -46,6 +63,9 @@ export default function QuizResult({ resultData }) {
                     </>
                 )
             })}
+
+            <audio ref={failSoundEffect} src='/sound/fail.mp3' />
+            <audio ref={successSoundEffect} src='/sound/success.mp3' />
         </>)
         }
 
